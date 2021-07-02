797c55f64c50aecf9dafc3eb4e3849d53793fd02
Dekpo <dekpo@me.com>
2021-07-02 11:14:49 +0200

    Added Register Inputs

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Input, Button } from 'react-native-elements';
 
 const RegisterScreen = () => {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [name,setName] = useState('');
+    const [imageUrl,setImageUrl] = useState('');
     return (
         <View style={styles.container}>
-            <Text>Register Screen</Text>
+            <Input
+                placeholder="Enter your name"
+                label='Name'
+                leftIcon={{ type: 'material', name: 'badge' }}
+                value={name}
+                onChangeText={text => setName(text)}
+            />
+             <Input
+                placeholder="Enter your email"
+                label='Email'
+                leftIcon={{ type: 'material', name: 'email' }}
+                value={email}
+                onChangeText={text => setEmail(text)}
+            />
+            <Input
+                placeholder="Enter your password"
+                label='Password'
+                leftIcon={{ type: 'material', name: 'lock' }}
+                value={password}
+                onChangeText={text => setPassword(text)}
+                secureTextEntry
+            />
+            <Input
+                placeholder="Enter your image url"
+                label='Profil Pic Url'
+                leftIcon={{ type: 'material', name: 'face' }}
+                value={imageUrl}
+                onChangeText={text => setImageUrl(text)}
+            />
+            <Button title="Register" onPress={() => alert('Register')} />
         </View>
     )
 }
