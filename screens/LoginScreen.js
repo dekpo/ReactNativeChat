a9130b082216166aec9b8a73e22542c231104032
Dekpo <dekpo@me.com>
2021-07-02 11:07:17 +0200

    Added Login Inputs

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Input, Button } from 'react-native-elements';
 
-const LoginScreen = ({navigation}) => {
+const LoginScreen = ({ navigation }) => {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
     return (
         <View style={styles.container}>
-            <Text>Login Screen</Text>
-            <Button title="Register" onPress={()=>navigation.navigate('Register')} />
+            <Input
+                placeholder="Enter your email"
+                label='Email'
+                leftIcon={{ type: 'material', name: 'email' }}
+                value={email}
+                onChangeText={text => setEmail(text)}
+            />
+            <Input
+                placeholder="Enter your password"
+                label='Password'
+                leftIcon={{ type: 'material', name: 'lock' }}
+                value={password}
+                onChangeText={text => setPassword(text)}
+                secureTextEntry
+            />
+            <Button title="Login" onPress={() => alert('Login')} />
+            <Button title="Register" onPress={() => navigation.navigate('Register')} />
         </View>
     )
 }
@@ -14,8 +32,8 @@ export default LoginScreen
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      alignItems: 'center',
-      padding: 10,
+        flex: 1,
+        alignItems: 'center',
+        padding: 10,
     },
-  });
+});
