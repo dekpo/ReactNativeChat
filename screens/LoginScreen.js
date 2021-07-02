841b333bacd413af1d9a7e1cc9acc0dccc81b2ff
Dekpo <dekpo@me.com>
2021-07-02 10:37:53 +0200

    Created navigation stack and LoginScreen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const LoginScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Login Screen</Text>
+        </View>
+    )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      padding: 10,
+    },
+  });
